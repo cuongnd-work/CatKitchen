7e43d283796fd5f0fb7f3279669a4fbce9fca0ec
CuongND <cuongnd@archergame.mobi>
2025-12-12 17:27:13 +0700

    implement state 1

--- assets/scripts/CatAnimationController.ts
+++ assets/scripts/CatAnimationController.ts
@@ -6,7 +6,25 @@ export class CatAnimationController extends Component {
     @property(SkeletalAnimation)
     private animation: SkeletalAnimation = null!;
 
+    public doIdle(){
+        this.animation.play("Dung");
+    }
 
+    public doWalk(){
+        this.animation.play("Warlk");
+    }
+
+    public doWalk_Angry(){
+        this.animation.play("Warlk_Angry");
+    }
+
+    public doBedo(){
+        this.animation.play("Bedo");
+    }
+
+    public doDoing(){
+        this.animation.play("Doing");
+    }
 }
 
 
