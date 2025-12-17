6a10490dce31bd614875e3d0b13510117e8bf27b
CuongND <cuongnd@archergame.mobi>
2025-12-17 15:49:46 +0700

    .

---
+++ assets/scripts/TusButton.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('TusButton')
+export class TusButton extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
